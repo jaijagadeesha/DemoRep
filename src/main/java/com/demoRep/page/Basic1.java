7c71b825eff47ff5158e40325f5493fba32df035
jaijagadeesha <jaijagadeesha@gmail.com>
2020-02-16 17:00:13 +0530

    first commit

---
+++ src/main/java/com/demoRep/page/Basic1.java
@@ -0,0 +1,12 @@
+package com.demoRep.page;
+
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+public class Basic1 {
+	public void test2()
+	{
+		System.out.println("test2 Rep");
+	}
+	
+
+}
---
+++ src/test/java/com/demoRep/test/Basic1PageTest.java
@@ -0,0 +1,9 @@
+package com.demoRep.test;
+
+public class Basic1PageTest {
+
+	public void login()
+	{
+		System.out.println("First Rep");
+	}
+}
